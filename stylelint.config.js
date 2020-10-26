c9768503614d79098f13948e6070863ebc06ef1c
torresdeleau web <estudiotorresdeleau.web@gmail.com>
2020-10-26 19:36:40 -0300

    Mas Correcciones de estilo

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,13 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'at-rule-no-unknown': null,
+    'comment-whitespace-inside': null,
+    'declaration-empty-line-before': null,
+    'value-keyword-case': null,
+    'selector-pseudo-element-colon-notation': null,
+    'no-duplicate-selectors': null,
+    'comment-empty-line-before': null,
+  },
 }
